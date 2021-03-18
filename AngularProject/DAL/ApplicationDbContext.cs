3f4988d4f184e82d2095c2dddb738045ac201ed6
Artem <artemermolaev0@gmail.com>
2021-03-18 13:37:12 +0200

    Add Ef

---
+++ AngularProject/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> User { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ AngularProject/DAL/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    interface IApplicationDbContext
+    {
+        public DbSet<User> User { get; set; }
+    }
+}
